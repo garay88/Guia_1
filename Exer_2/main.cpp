2d5ab2497b26bb05a6b30f82423f48ba6c053457
garay88 <emmanuel.garay@udea.edu.co>
2021-03-20 16:35:04 -0500

    Solucion ejercio 2

---
+++ Exer_2/main.cpp
@@ -0,0 +1,25 @@
+/*
+Programa que pide un numero N e imprime si es par o impar
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int NumN;   //Numero N
+
+    cout<<"Digite el numero a evaluar: ";
+    cin>>NumN;
+
+    if (NumN%2==0)
+    {
+        cout<<"El numero "<<NumN<<" es PAR."<<endl;
+    }
+
+    else
+    {
+        cout<<"El numero "<<NumN<<" es IMPAR."<<endl;
+    }
+}
