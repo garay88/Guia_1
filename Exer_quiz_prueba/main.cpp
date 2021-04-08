f776389a3a2ce67993bb814418d89430e44276a3
garay88 <emmanuel.garay@udea.edu.co>
2021-04-08 18:20:03 -0500

    Se añade la parte de potencia

--- Exer_quiz_prueba/main.cpp
+++ Exer_quiz_prueba/main.cpp
@@ -9,17 +9,34 @@ using namespace std;
 int main()
 {
   int Num;
+  int i;
   int Sum=0;
-  int Conter=0;
+  int Pow=0;
+  int Potencia=1;
+  int total;
+  int Conter=1;
 
-  cout<<"Ingrese el numero a evaluar: "<<end;
+  cout<<"Ingrese el numero a evaluar: ";
   cin>>Num;
 
-  for (int i=10;;i=i*10)    //se utiliza para saber la cantidad de digitos
+  for (i=10;;i=i*10)    //se utiliza para saber la cantidad de digitos
   {
-        if (Num/i<1){break;}
+        if (Num/i<1 && Num/i>-1){break;}
         Conter++;
   }
 
+  i=i/10;
+
+  while (i>=0)
+  {
+      Sum=Num/i;
+      Pow=Sum;
+
+      while (Pow > 1)
+      {
+          Potencia=Potencia*Sum;
+          Pow--;
+      }
+  }
   return 0;
 }
