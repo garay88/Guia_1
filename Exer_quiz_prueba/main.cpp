27c6338d7863701c22ef9fa92307af1dac223996
garay88 <emmanuel.garay@udea.edu.co>
2021-04-08 17:59:19 -0500

    Se crearon cositas

--- Exer_quiz_prueba/main.cpp
+++ Exer_quiz_prueba/main.cpp
@@ -1,9 +1,25 @@
+/* Programa que pide un numero entero N e imprime el
+   resultado de la suma de todos sus digitos elevados
+   a si mismos */
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+  int Num;
+  int Sum=0;
+  int Conter=0;
+
+  cout<<"Ingrese el numero a evaluar: "<<end;
+  cin>>Num;
+
+  for (int i=10;;i=i*10)    //se utiliza para saber la cantidad de digitos
+  {
+        if (Num/i<1){break;}
+        Conter++;
+  }
+
+  return 0;
 }
